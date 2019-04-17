5c92d02088f536c451184f369519b43f4d2629de
Sławomir Zborowski <slawomir.zborowski@nokia.com>
2019-04-17 23:21:39 +0200

    added most basic scenario for querying along with trivial indexing mechanism
    Signed-off-by: Sławomir Zborowski <slawomir.zborowski@nokia.com>

---
+++ server/index_log.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+import argparse
+import json
+import sqlite3
+import pathlib
+
+
+def index_log(input_path, output_path):
+    if pathlib.Path(output_path).exists():
+        raise RuntimeError(f'file {output_path} already exists!')
+    conn = sqlite3.connect(output_path)
+
+    CREATE_TABLE = \
+    '''
+    CREATE TABLE logs (
+        tags varchar(255),
+        labels varchar(512),
+        timestamp int,
+        line text NOT NULL
+    );
+    '''
+    c = conn.cursor()
+    c.execute(CREATE_TABLE)
+
+    with open(input_path) as f:
+        for line in f:
+            j = json.loads(line)
+            tags, labels, timestamp, line = ','.join(j['tags']), ','.join(j['labels']), j['timestamp'], j['line']
+            c.execute(f'INSERT INTO logs (tags, labels, timestamp, line) VALUES ("{tags}", "{labels}", "{timestamp}", "{line}")')
+        conn.commit()
+
+    conn.close()
+
+if __name__ == '__main__':
+    ap = argparse.ArgumentParser()
+    ap.add_argument('input_file')
+    ap.add_argument('output_file')
+    args = ap.parse_args()
+    index_log(args.input_file, args.output_file)
--- server/server.py
+++ server/server.py
@@ -2,13 +2,34 @@
 
 import connexion
 from flask import Flask, jsonify
+import sqlite3
+import pathlib
 
 
 app = Flask(__name__)
+DATA_DIR = pathlib.Path('./data')
 
 
 def query(log_id):
-    return jsonify({'hello': 'world'})
+    path = DATA_DIR / f'{log_id}.sqlite'
+    if not path.exists():
+        return jsonify({'error': 'log not found'}), 404
+    conn = sqlite3.connect(str(path))
+    conn.enable_load_extension(True)
+    conn.load_extension('/usr/lib/sqlite3/pcre.so')
+    cursor = conn.cursor()
+
+    filters_str = ''
+    size = 5000
+    offset = 0
+
+    if filters_str:
+        where_clause = 'WHERE ' + filters_str
+    else:
+        where_clause = ''
+
+    q = f"SELECT line FROM logs {where_clause} LIMIT {size} OFFSET {offset}"
+    return jsonify({"log_lines": '\n'.join([x[0] for x in cursor.execute(q).fetchall()])})
 
 
 def _main():
