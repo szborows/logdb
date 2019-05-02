ed27953537f9a6f266161a8e557551ec04d143db
Sławomir Zborowski <slawomir.zborowski@nokia.com>
2019-05-02 19:25:27 +0200

    [experiment: swim] added scratch of controller code
    Signed-off-by: Sławomir Zborowski <slawomir.zborowski@nokia.com>

--- server/experiments/swim/main.py
+++ server/experiments/swim/main.py
@@ -13,10 +13,16 @@ class MyProtocol(asyncio.DatagramProtocol):
     def datagram_received(self, data, addr):
         print(data)
 
-async def _init():
+async def _run_controller():
+    while True:
+        await asyncio.sleep(1)
+        print('alive')
+
+async def _init(loop):
     await loop.create_datagram_endpoint(MyProtocol,local_addr=('127.0.0.1', 9999))
+    asyncio.ensure_future(_run_controller())
 
 if __name__ == '__main__':
     loop = asyncio.get_event_loop()
-    loop.run_until_complete(_init())
+    loop.run_until_complete(_init(loop))
     loop.run_forever()
